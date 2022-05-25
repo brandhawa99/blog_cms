34edaa7869f957a71d911a05284b29d39931e0bd
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-24 23:12:19 -0700

    moved stuff to their own files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import {BrowserRouter as Router, Route, Routes,  } from 'react-router-dom'
+import {useEffect} from 'react';
+import Nav from './Components/Nav'
+import Posts from './Components/Posts'
+import Auth from './Components/Auth'
 
 function App() {
+  const [login, setLogin] = useState(true);
+
+
+  const switch_form = () =>{
+    setLogin(!login);
+  }
+  useEffect(() =>{
+    const token = localStorage.getItem('token')
+    if(token !== null){
+
+    }else{
+    }
+
+
+  },[])
+
+
+
+  // const signupForm = () =>{
+  //   return (
+  //     <div>
+  //       <form action='http://localhost:3001/auth/signup' method='POST'>
+  //         <label>First Name</label>
+  //         <input type='text' name="first_name" placeholder='First Name'/>
+  //         <label>Last Name</label>
+  //         <input type='text' name="last_name" />
+  //         <label>username</label>
+  //         <input type='text' name="username" />
+  //         <label>password</label>
+  //         <input type='password' name="password"/>
+  //         <label>confirm password</label>
+  //         <input type='password' name="password2" />
+  //         <button type='submit'>Sign Up</button>
+
+  //       </form>
+  //     </div>
+  //   )
+  // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Router>
+        <Routes>
+          <Route path = '/' element={<Auth />} />
+          <Route path='/author/posts' element={<Posts />}/>
+        </Routes>
+      </Router>
+
+
+
     </div>
   );
 }
