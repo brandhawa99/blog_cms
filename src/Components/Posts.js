15f8a9a390d0f65091203a89b234d4dcae1f9afd
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-26 10:14:53 -0700

    added check for an okay response

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -2,6 +2,7 @@ import {React, useState, useEffect} from 'react'
 
 export default function Posts(props) {
   const [postData, setPostsData] = useState(undefined);
+  const [errors, setErrors] = useState(false);
 
   const data =async() =>{
 
@@ -19,14 +20,18 @@ export default function Posts(props) {
       // if(!response.ok){
       //   throw new Error('There was an error')
       // }
-      const data = await response.json();
-      console.log(data);
-      setPostsData(JSON.parse(JSON.stringify(data)));
+      if(response.ok){
+
+        const data = await response.json();
+        console.log(data);
+        setPostsData(JSON.parse(JSON.stringify(data)));
+      }else{
+        throw new Error('The response was not okay')
+      }
       
     } catch (error) {
       console.log(error);
-      console.log("THERE WAS AN ERROR");
-      
+
     }
 
   }
