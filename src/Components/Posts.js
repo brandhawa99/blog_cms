9f57e5d814a79d453482101327ed9e19c32884cd
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-25 18:34:31 -0700

    added post logic

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,32 +1,44 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 
-export default function Posts() {
+export default function Posts(props) {
+  const [postData, setPostsData] = useState(undefined);
 
-  const getProtectedRoute = async() =>{
-    const idToken = localStorage.getItem('token');
-    try{
+  const data =async() =>{
+
+    try {
+      const idToken = localStorage.getItem('token');
       const response = await fetch('http://localhost:3001/author/posts',{
-        method:"GET",
+        method: 'GET',
+        mode:'cors',
         headers:{
-          "Authorization" : idToken
-        },
-  
-      })
-      if(!response.ok){
-        throw new Error("You are not authorized")
-      }
-      const data = response.json();
-      console.log(data);
-    }catch(err){
-      console.err('thee was an erro', err);
+          'Authorization' : idToken, 
 
+        }
+      });
+
+      // if(!response.ok){
+      //   throw new Error('There was an error')
+      // }
+      const data = await response.json();
+      console.log(data);
+      setPostsData(JSON.parse(JSON.stringify(data)));
+      
+    } catch (error) {
+      console.log(error);
+      console.log("THERE WAS AN ERROR");
+      
     }
 
   }
 
+  useEffect(() =>{
+    data();
+  },[])
 
 
   return (
-    <div>This is an authorized route</div>
+    <div>
+      {props.userAuth ? <div>yes</div>:<div>no</div>}
+    </div>
   )
 }
