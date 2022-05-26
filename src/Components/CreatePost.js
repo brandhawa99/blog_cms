51718e30ef880caa46aa1c15f3152ed0b063466b
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-26 10:16:19 -0700

    added switch statement to check if logged in

--- src/Components/CreatePost.js
+++ src/Components/CreatePost.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
-export default function CreatePost() {
+function CreatePost(props) {
   return (
-    <div>
+  <div>
+    {
+      props.userAuth ?
       <form>
-        <div>
+      <div>
         <label>Private</label>
         <input name='private' type='checkbox'></input>
-        </div>
-        <input type='text' placeholder='Title' name='title'/ >
-        <textarea type='textarea' placeholder='post' name='post'/ >
-        <button>Post</button>
-      </form>
-    </div>
+      </div>
+      <input type='text' placeholder='Title' name='title' />
+      <textarea type='textarea' placeholder='post' name='post'/>
+      <button>Post</button>
+    </form>:<div> LOGIN</div>
+    }
+  </div>
   )
 }
+
+export default CreatePost
+
