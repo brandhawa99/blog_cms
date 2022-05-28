7242b7379116daa23d62ed74db02b32ad2076ef5
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-27 20:01:41 -0700

    created list of author posts

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,6 +1,12 @@
 import {React, useState, useEffect} from 'react'
+import Post from './Post'
+import {v4 as uuid} from 'uuid';
+import { useNavigate } from 'react-router-dom';
+import '../styles/Post.css'
 
 export default function Posts(props) {
+  const navigate = useNavigate();
+  
   const [postData, setPostsData] = useState(undefined);
   const [errors, setErrors] = useState(false);
 
@@ -17,14 +23,13 @@ export default function Posts(props) {
         }
       });
 
-      // if(!response.ok){
-      //   throw new Error('There was an error')
-      // }
       if(response.ok){
 
-        const data = await response.json();
-        console.log(data);
+        let data = await response.json();
+        //get the array from the object
+        data = data.posts;
         setPostsData(JSON.parse(JSON.stringify(data)));
+
       }else{
         throw new Error('The response was not okay')
       }
@@ -42,8 +47,17 @@ export default function Posts(props) {
 
 
   return (
-    <div>
-      {props.userAuth ? <div>yes</div>:<div>no</div>}
+    <div className='main-container'>
+
+      {postData?.map(post =>{
+        return(
+          <div key={uuid()}className={`post-container ${post.public?'green':'red'}`}onClick={()=>{navigate('/author/posts/'+post._id)}}>    
+          <Post key={post._id} title={post.title} blog={post.blog} public={post.public} timestamp={post.timestamp} />
+          </div>
+        )  
+
+
+      })}
     </div>
   )
 }
