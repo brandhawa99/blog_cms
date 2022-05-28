c2204dd5c55db951fbce1ead2494e2f92e35f9ce
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-27 20:00:51 -0700

    added submit for form

--- src/Components/CreatePost.js
+++ src/Components/CreatePost.js
@@ -1,6 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CreatePost(props) {
+  const [formData, setFormData] = useState({
+    title:"",
+    blog:"",
+    private:false,
+  })
+  const handleLogin = (e)=>{
+    e.preventDefault();
+    const newdata = {...formData};
+    newdata[e.target.name] = e.target.value;
+    setFormData(newdata);
+  }
+
+  const submit_Form = async() =>{
+    try {
+
+      const response = fetch('http://localhost:3001/author/post/create',{
+        method : 'POST',
+        mode: 'cors',
+        credentials:'same-origin',
+        headers:{
+          'Content-Type':'application/json',
+          'Authorization': localStorage.getItem('token'),
+        },
+        body:JSON.stringify({formData}),
+      });
+      if(response.ok){
+
+      }
+      
+    } catch (error) {
+      
+    }
+  }
+
+
+
   return (
   <div>
     {
@@ -12,7 +48,7 @@ function CreatePost(props) {
       </div>
       <input type='text' placeholder='Title' name='title' />
       <textarea type='textarea' placeholder='post' name='post'/>
-      <button>Post</button>
+      <button onClick={submit_Form}>Post</button>
     </form>:<div> LOGIN</div>
     }
   </div>
