7eb8b6795320875e503ada1cd3f59f8c763c5c41
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-30 21:48:54 -0700

    added classnames

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,13 +1,15 @@
 import React from 'react'
+require('../styles/Post.css')
 
 function Post(props) {
   const {title,blog,timestamp} = props
+  var options = {year: 'numeric', month: 'long', day: 'numeric' };
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{blog}</p>
-      <p>{timestamp}</p>
+    <div className='post-thing'>
+      <h1 className='title'>{title}</h1>
+      <p className='post-blog'>{blog}</p>
+      <p>{new Date(timestamp).toLocaleTimeString('en-US',options)}</p>
       <p>{props.public}</p>
     </div>
   )
