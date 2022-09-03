5a271b7a75f1fe6a3cc6193c8cb0334ef3145c79
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-09-02 20:08:40 -0700

    added generate react app

---
+++ generate-templates/TemplateName.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./TemplateName.module.css";
+
+function TemplateName() {
+  return (
+    <div className={styles.mainContainer}>
+      TemplateName
+      <div></div>
+    </div>
+  );
+}
+
+export default TemplateName;
